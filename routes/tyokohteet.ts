a61477cc72d797ae59195b4ac55f62af960a4969
Johannes Dahlin <dahlin.johannes@hotmail.com>
2026-04-15 16:05:23 +0300

    työkohteet routen lisäys

--- routes/tyokohteet.ts
+++ routes/tyokohteet.ts
@@ -1,11 +1,41 @@
 import { Router, type Request, type Response } from 'express';
+import * as tyokohdeModel from '../models/tyokohde';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-      res.render('tyokohteet/tyokohteet', { 
-        title: 'Työkohteet',
-    });
-})
+// 1. REITTI: Työkohdelista (GET /tyokohteet)
+router.get('/', async (req: Request, res: Response) => {
+  const tyokohteet = await tyokohdeModel.getAll();
+
+  if (!tyokohteet || tyokohteet.length === 0) {
+    return res.status(404).send('Ei työkohteita');
+  }
+
+  res.render('tyokohteet/tyokohteet', {
+    title: 'Työkohteet',
+    tyokohteet: tyokohteet
+  });
+});
+
+// 2. REITTI: Yksittäinen työkohde (GET /tyokohteet/:id)
+router.get('/:id', async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const tyokohde = await tyokohdeModel.getById(id);
+
+  if (!tyokohde) {
+    return res.status(404).send('Työkohdetta ei löydy');
+  }
+
+  res.render('tyokohteet/tyokohde', {
+    title: tyokohde.nimi,
+    tyokohde: tyokohde
+  });
+});
+
+// 3. REITTI: Lisää työkohde (T1)
+router.post('/lisaa', async (req: Request, res: Response) => {
+  await tyokohdeModel.add(req.body);
+  res.redirect('/asiakkaat/' + req.body.asiakas_id);
+});
 
 export default router;
